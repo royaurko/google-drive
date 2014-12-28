8a0118b698083fe3b62b076a9bf6b7207cd0a53f
aurko <roy.aurko@gmail.com>
2014-12-28 17:46:31 -0500

    added try, except

--- local2remote.py
+++ local2remote.py
@@ -12,50 +12,52 @@ from apiclient import errors
 
 def upload(file_name, drive_service, json_info, log_file, flag=True, parent_id=None):
     # Function to upload a file
-    # Before uploading check if the file already exists in drive
-    k = file_name.rfind('/') + 1
-    fname = file_name[k:]
-    cursor = json_info.find({'title': fname, 'path': file_name})
-    if cursor.count() > 1:
-        return json_info
-    if flag:
-        mime_type = mimetypes.guess_type(file_name)
-        if mime_type == (None, None):
-            mime_type = 'text/plain'
-        media_body = MediaFileUpload(file_name,
-                                     mimetype=mime_type, resumable=True)
-    else:
-        media_body = None
-        mime_type = 'application/vnd.google-apps.folder'
-    body = {
-        'title': fname,
-        'mimeType': mime_type
-    }
-    if parent_id:
-        body['parents'] = [{'id': parent_id}]
-    # Upload the returned metadata to mongodb
     try:
-        file = drive_service.files().insert(body=body,
-                                            media_body=media_body).execute()
-        temp_dict = dict((k, file[k])
-                         for
-                         k
-                         in
-                         ('id', 'title', 'parents',
-                          'mimeType', 'createdDate', 'modifiedDate'))
-        temp_dict['path'] = file_name
-        temp_dict['broken'] = False
-        write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
+        # Before uploading check if the file already exists in drive
+        k = file_name.rfind('/') + 1
+        fname = file_name[k:]
+        cursor = json_info.find({'title': fname, 'path': file_name})
+        if cursor.count() > 1:
+            return json_info
         if flag:
-            write_str += 'Uploaded (new) file: ' + file_name + '\n'
-            log_file.write(write_str)
+            mime_type = mimetypes.guess_type(file_name)
+            if mime_type == (None, None):
+                mime_type = 'text/plain'
+            media_body = MediaFileUpload(file_name, mimetype=mime_type, resumable=True)
         else:
-            write_str += 'Uploaded (new) directory: ' + file_name + '\n'
-            log_file.write(write_str)
-        json_id = json_info.insert(temp_dict)
-    except errors.HttpError, error:
-        print 'An error occured: %s' % error
-    return json_info
+            media_body = None
+            mime_type = 'application/vnd.google-apps.folder'
+        body = {
+            'title': fname,
+            'mimeType': mime_type
+        }
+        if parent_id:
+            body['parents'] = [{'id': parent_id}]
+        # Upload the returned metadata to mongodb
+        try:
+            file = drive_service.files().insert(body=body, media_body=media_body).execute()
+            temp_dict = dict((k, file[k])
+                             for
+                             k
+                             in
+                             ('id', 'title', 'parents',
+                             'mimeType', 'createdDate', 'modifiedDate'))
+            temp_dict['path'] = file_name
+            temp_dict['broken'] = False
+            write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
+            if flag:
+                write_str += 'Uploaded (new) file: ' + file_name + '\n'
+                log_file.write(write_str)
+            else:
+                write_str += 'Uploaded (new) directory: ' + file_name + '\n'
+                log_file.write(write_str)
+            json_id = json_info.insert(temp_dict)
+        except errors.HttpError, error:
+            print 'An error occured: %s' % error
+        return json_info
+    except:
+        print 'An error occured uploading file\n'
+        return json_info
 
 
 def update(file_name, drive_service, json_info, log_file):
@@ -93,58 +95,70 @@ def update(file_name, drive_service, json_info, log_file):
         write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
         write_str += 'Uploaded (modified) file: ' + file_name + '\n'
         log_file.write(write_str)
+        return json_info
     except errors.HttpError, error:
         print 'An error has occured: %s' % error
-    return json_info
+        return json_info
+    except:
+        print 'An error occurred updating file\n'
+        return json_info
 
 
 def delete(file_name, drive_service, json_info, log_file):
-    # Delete individual file or folder
-    k = file_name.rfind('/') + 1
-    fname = file_name[k:]
-    cursor = json_info.find({'title': fname, 'path': file_name})
-    for entry in cursor:
-        # delete all files with matching file_name (includes path)
-        file_id = entry['id']
-        try:
-            drive_service.files().delete(fileId=file_id).execute()
-            json_info.remove({'id': entry['id']})
-            write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
-            write_str += 'Deleted: ' + file_name + '\n'
-            log_file.write(write_str)
-        except errors.HttpError, error:
-            print 'An error occurred: %s' % error
-    return json_info
+    try:
+        # Delete individual file or folder
+        k = file_name.rfind('/') + 1
+        fname = file_name[k:]
+        cursor = json_info.find({'title': fname, 'path': file_name})
+        for entry in cursor:
+            # delete all files with matching file_name (includes path)
+            file_id = entry['id']
+            try:
+                drive_service.files().delete(fileId=file_id).execute()
+                json_info.remove({'id': entry['id']})
+                write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
+                write_str += 'Deleted: ' + file_name + '\n'
+                log_file.write(write_str)
+            except errors.HttpError, error:
+                print 'An error occurred: %s' % error
+        return json_info
+    except:
+        print 'Error deleting file\n'
+        return json_info
 
 
 def purge(file_name, drive_service, json_info, log_file):
     # Delete file or folder and all the resulting orphans
-    cursor = json_info.find({'path': file_name})
-    if cursor.count() == 0:
-        return json_info
-    for entry in cursor:
-        file_id = entry['id']
-        if entry['mimeType'] != 'application/vnd.google-apps.folder':
-            #It is a file and so no need to check for orphans
-            json_info = delete(file_name, drive_service, json_info, log_file)
+    try:
+        cursor = json_info.find({'path': file_name})
+        if cursor.count() == 0:
             return json_info
-    # It must be a folder, check for possible orphans
-    delete_info = set([(file_name, file_id)])
-    flag = True
-    while flag:
-        cursor = json_info.find()
-        old_size = len(delete_info)
         for entry in cursor:
-            if 'parents' in entry:
-                if entry['parents']:
-                    delete_id = set([f[1] for f in delete_info])
-                    if entry['parents'][0]['id'] in delete_id:
-                        # It has a parent in the list of items to be deleted
-                        delete_info.add((entry['path'], entry['id']))
-        if len(delete_info) == old_size:
-            # No new children found
-            flag = False
-    # Delete all the files and folders accumulated in delete_id
-    for f in delete_info:
-         json_info = delete(f[0], drive_service, json_info, log_file)
-    return json_info
+            file_id = entry['id']
+            if entry['mimeType'] != 'application/vnd.google-apps.folder':
+                #It is a file and so no need to check for orphans
+                json_info = delete(file_name, drive_service, json_info, log_file)
+                return json_info
+        # It must be a folder, check for possible orphans
+        delete_info = set([(file_name, file_id)])
+        flag = True
+        while flag:
+            cursor = json_info.find()
+            old_size = len(delete_info)
+            for entry in cursor:
+                if 'parents' in entry:
+                    if entry['parents']:
+                        delete_id = set([f[1] for f in delete_info])
+                        if entry['parents'][0]['id'] in delete_id:
+                            # It has a parent in the list of items to be deleted
+                            delete_info.add((entry['path'], entry['id']))
+            if len(delete_info) == old_size:
+                # No new children found
+                flag = False
+        # Delete all the files and folders accumulated in delete_id
+        for f in delete_info:
+            json_info = delete(f[0], drive_service, json_info, log_file)
+        return json_info
+    except:
+        print 'Error deleting file\n'
+        return json_info
--- remote2local.py
+++ remote2local.py
@@ -9,124 +9,143 @@ import time
 from apiclient import errors
 
 def download_dir(path, drive_service, json_info, mongo_id, log_file):
-    json_info.update({'_id': mongo_id}, {"$set": {'path': path}})
-    if not os.path.exists(path):
-        write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
-        write_str += 'Downloaded directory: ' + path + '\n'
-        log_file.write(write_str)
-        os.makedirs(path)
-    return json_info
+    try:
+        json_info.update({'_id': mongo_id}, {"$set": {'path': path}})
+        if not os.path.exists(path):
+            write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
+            write_str += 'Downloaded directory: ' + path + '\n'
+            log_file.write(write_str)
+            os.makedirs(path)
+        return json_info
+    except:
+        print 'An error occurred downloading directory\n'
+        return json_info
 
 
 def download_file(path, drive_service, json_info, file_id, log_file, parent_id=None):
     try:
-        file = drive_service.files().get(fileId=file_id).execute()
-    except errors.HttpError, error:
-        print 'An error occured: %s' % error
+        try:
+            file = drive_service.files().get(fileId=file_id).execute()
+        except errors.HttpError, error:
+            print 'An error occured: %s' % error
+            return json_info
+        download_url = file.get('downloadUrl')
+        # In case the directory structure doesn't exist
+        if not os.path.exists(path):
+            if parent_id is not None:
+                mongo_id = json_info.find_one({'id': parent_id})['_id']
+                download_dir(path, drive_service, json_info, mongo_id, log_file)
+        title = path + '/' + file.get('title')
+        if download_url:
+            resp, content = drive_service._http.request(download_url)
+            if resp.status == 200:
+                # need to set path properly
+                if not os.path.isfile(title):
+                    f = open(title, 'w')
+                    f.write(content)
+                    f.close()
+                    write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
+                    write_str += 'Downloaded file: ' + title + '\n'
+                    log_file.write(write_str)
+                    json_info.update({'id': file_id}, {"$set": {'path': title}})
+            else:
+                print 'An error occurred: %s' % resp
         return json_info
-    download_url = file.get('downloadUrl')
-    # In case the directory structure doesn't exist
-    if not os.path.exists(path):
-        if parent_id is not None:
-            mongo_id = json_info.find_one({'id': parent_id})['_id']
-            download_dir(path, drive_service, json_info, mongo_id, log_file)
-    title = path + '/' + file.get('title')
-    if download_url:
-        resp, content = drive_service._http.request(download_url)
-        if resp.status == 200:
-            # need to set path properly
-            if not os.path.isfile(title):
-                f = open(title, 'w')
-                f.write(content)
-                f.close()
-                write_str = time.strftime("%m.%d.%y %H:%M ", time.localtime())
-                write_str += 'Downloaded file: ' + title + '\n'
-                log_file.write(write_str)
-                json_info.update({'id': file_id}, {"$set": {'path': title}})
-        else:
-            print 'An error occurred: %s' % resp
-    return json_info
+    except:
+        print 'An error occurred downloading file\n'
+        return json_info
 
 
 def mirror_dir(cursor, path, drive_service, json_info, log_file):
-    for entry in cursor:
-        if entry['parents']:
-            # Only download folders that have well defined parents
-            if entry['mimeType'] == 'application/vnd.google-apps.folder':
-                # Only download folders
-                if entry['parents'][0]['isRoot']:
-                    # These folders are in the root directory
-                    dir_path = path + '/' + entry['title']
-                    mongo_id = entry['_id']
-                    json_info = download_dir(dir_path, drive_service, json_info, mongo_id, log_file)
-                else:
-                    # These are subdirectories of directories inside the root
-                    current_id = entry['id']
-                    dir_path = ''
-                    flag = False
-                    broken = False
-                    while not flag:
-                        # Attempts to find the relative path by examining
-                        # ancestors
-                        current_entry = json_info.find_one({'id': current_id})
-                        if current_entry is None:
-                            json_info.update({'id': current_id}, {"$set": {'broken': True}})
-                            broken = True
-                            break
-                        if 'parents' in current_entry:
-                            if current_entry['parents']:
-                                # Update flag to that of its parent
-                                dir_path = current_entry['title'] + '/' + dir_path
-                                current_id = current_entry['parents'][0]['id']
-                                flag = current_entry['parents'][0]['isRoot']
-                            else:
-                                #Broken link
-                                json_info.update({'id': current_id}, {"$set": {'broken': True}})
-                                broken = True
-                                break
-                        else:
-                            # Broken link
-                            json_info.update({'id': current_id}, {"$set": {'broken': True}})
-                            broken = True
-                            break
-                    if not broken:
-                        dir_path = path + '/' + dir_path
+    try:
+        for entry in cursor:
+            if entry['parents']:
+                # Only download folders that have well defined parents
+                if entry['mimeType'] == 'application/vnd.google-apps.folder':
+                    # Only download folders
+                    if entry['parents'][0]['isRoot']:
+                        # These folders are in the root directory
+                        dir_path = path + '/' + entry['title']
                         mongo_id = entry['_id']
                         json_info = download_dir(dir_path, drive_service, json_info, mongo_id, log_file)
                     else:
-                        json_info.update({'id': entry['id']}, {"$set": {'broken': True}})
-    tmp_cursor = json_info.find({'broken': True, 'mimeType': 'application/vnd.google-apps.folder'})
+                        # These are subdirectories of directories inside the root
+                        current_id = entry['id']
+                        dir_path = ''
+                        flag = False
+                        broken = False
+                        while not flag:
+                            # Attempts to find the relative path by examining
+                            # ancestors
+                            current_entry = json_info.find_one({'id': current_id})
+                            if current_entry is None:
+                                json_info.update({'id': current_id}, {"$set": {'broken': True}})
+                                broken = True
+                                break
+                            if 'parents' in current_entry:
+                                if current_entry['parents']:
+                                    # Update flag to that of its parent
+                                    dir_path = current_entry['title'] + '/' + dir_path
+                                    current_id = current_entry['parents'][0]['id']
+                                    flag = current_entry['parents'][0]['isRoot']
+                                else:
+                                    #Broken link
+                                    json_info.update({'id': current_id}, {"$set": {'broken': True}})
+                                    broken = True
+                                    break
+                            else:
+                                # Broken link
+                                json_info.update({'id': current_id}, {"$set": {'broken': True}})
+                                broken = True
+                                break
+                        if not broken:
+                            dir_path = path + '/' + dir_path
+                            mongo_id = entry['_id']
+                            json_info = download_dir(dir_path, drive_service, json_info, mongo_id, log_file)
+                        else:
+                            json_info.update({'id': entry['id']}, {"$set": {'broken': True}})
+    except:
+        print 'An error occurred mirroring directories\n'
+        return json_info
 
 
 def mirror_file(cursor, path, drive_service, json_info, log_file):
-    for entry in cursor:
-        if entry['parents']:
-            # Do not download objects without parent attribute
-            if entry['mimeType'] != 'application/vnd.google-apps.folder':
-                # If it is not a folder
-                if entry['parents'][0]['isRoot']:
-                    # If it is in the root
-                    parent_id = entry['parents'][0]['id']
-                    json_info = download_file(path, drive_service, json_info, entry['id'], log_file, parent_id=parent_id)
-                else:
-                    # Figure out its parent, see if it is downloaded already
-                    parent_id = entry['parents'][0]['id']
-                    parent_info = json_info.find_one({'id': parent_id})
-                    if parent_info is not None:
-                        if 'path' in parent_info and 'broken' in parent_info:
-                            if parent_info['path'] is not None and not parent_info['broken']:
-                                # This means the parent has already been downloaded
-                                mongo_id = entry['_id']
-                                json_info = download_file(parent_info['path'], drive_service, json_info, log_file, entry['id'])
-    return json_info
+    try:
+        for entry in cursor:
+            if entry['parents']:
+                # Do not download objects without parent attribute
+                if entry['mimeType'] != 'application/vnd.google-apps.folder':
+                    # If it is not a folder
+                    if entry['parents'][0]['isRoot']:
+                        # If it is in the root
+                        parent_id = entry['parents'][0]['id']
+                        json_info = download_file(path, drive_service, json_info, entry['id'], log_file, parent_id=parent_id)
+                    else:
+                        # Figure out its parent, see if it is downloaded already
+                        parent_id = entry['parents'][0]['id']
+                        parent_info = json_info.find_one({'id': parent_id})
+                        if parent_info is not None:
+                            if 'path' in parent_info and 'broken' in parent_info:
+                                if parent_info['path'] is not None and not parent_info['broken']:
+                                    # This means the parent has already been downloaded
+                                    mongo_id = entry['_id']
+                                    json_info = download_file(parent_info['path'], drive_service, json_info, log_file, entry['id'])
+        return json_info
+    except:
+        print 'An error occurred mirroring files\n'
+        return json_info
 
 
 def mirror(path, drive_service, json_info, log_file):
-    # Mirror remote content into the folder pointed by path
-    cursor = json_info.find({'mimeType':'application/vnd.google-apps.folder'})
-    # first download all the directories
-    mirror_dir(cursor, path, drive_service, json_info, log_file)
-    # Download files to appropriate folders
-    cursor = json_info.find()
-    mirror_file(cursor, path, drive_service, json_info, log_file)
-    return json_info
+    try:
+        # Mirror remote content into the folder pointed by path
+        cursor = json_info.find({'mimeType':'application/vnd.google-apps.folder'})
+        # first download all the directories
+        mirror_dir(cursor, path, drive_service, json_info, log_file)
+        # Download files to appropriate folders
+        cursor = json_info.find()
+        mirror_file(cursor, path, drive_service, json_info, log_file)
+        return json_info
+    except:
+        print 'An error occurred mirroring\n'
+        return json_info
